985ec2655c4b4c4ffc7deb6b5a0b1b98973eaa79
Daniel German <danielchuksgerman@gmail.com>
2024-06-27 10:52:43 +0100

    MongoDB utils

--- utils/redis.js
+++ utils/redis.js
@@ -2,33 +2,33 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
+  constructor() {
+    this.client = createClient();
 
-        this.client.on('error', (err) => {
-            console.error(err);
-        });
+    this.client.on('error', (err) => {
+      console.error(err);
+    });
 
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return await this.getAsync(key);
-    }
+  async get(key) {
+    return await this.getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        await this.setAsync(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        await this.delAsync(key);
-    }
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
